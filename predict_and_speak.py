1d600d4d97e49ab07b10c3f376e58b379bbef155
Long Nguyen <LongNguyen@MacBook-Pro-3.local>
2017-06-26 17:45:48 +0900

    Added specs, TODO: track gesture times

--- predict_and_speak.py
+++ predict_and_speak.py
@@ -8,19 +8,43 @@ nlp = eng.load()
 model = pickle.load(open('pickles/perceptron.pickle', 'rb'))
 
 def blank():
+    '''
+    Prints 1000 blank lines, effectively
+    clearing the console.
+    '''
     print '\n' * 1000
 
 def process_gesture(category):
+    '''
+    Processes one of the nine categories
+    grouping NAO gestures together.
+    
+    :param category: Category to explore and generate
+                     a behavioral gesture from.
+    :return: A random animation fitting the appropriate
+             category, ensuring "natural", stochastic output.
+    '''
     group = read_animations.grouped_tags[category]
     animations = read_animations.animations_by_tag[group[randint(0, len(group) - 1)]]
     return animations[randint(0, len(animations) - 1)]
 
 def process_input(input):
+    '''
+
+
+    :param input:
+    :return:
+    '''
     prediction = model.predict(nlp(unicode(input)).vector.reshape(1, -1))
     gesture = process_gesture(prediction[0])  # 0th index is gesture tag.
-    return '^mode(disabled) ' + start(gesture) + ' ' + input + ' ' + wait(gesture)
+    return start(gesture) + ' ' + input + ' ' + wait(gesture)
 
 def prompt():
+    '''
+    
+
+    :return:
+    '''
     messages = []
     blank()
     print 'Please input messages for NAO to speak and behave with.'
@@ -33,8 +57,9 @@ def prompt():
         messages.append(process_input(input))
     return messages
 
+# Dump the messages to disk & call subprocess for NAO to speak.
 pickle.dump(prompt(), open('pickles/messages.pickle', 'wb'))
 blank()
-print 'Saved messages to disk. Speaking now...'
+print 'Saved messages to disk. Speaking now...\n'
 
 call(['python', 'speech_runner.py'])
